cafd1a62fcf9bc7565c8830767014e82ce0db940
Rasec <elch.herrera@gmail.com>
2021-02-01 17:56:01 -0500

    add button logic

--- src/App.js
+++ src/App.js
@@ -11,8 +11,32 @@ class App extends Component {
       todos: [
         'Learn React',
         'Do Laundry'
-      ]
+      ],
+      todoToAdd:""
     };
+  
+    //this binding is neccesary to make 'this' work in the callback
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+
+  handleChange(event){
+    const nextTodoToAdd = event.target.value;
+
+    this.setState({
+      todoToAdd: nextTodoToAdd
+    });
+  }
+
+  handleClick(event){
+    const nextTodos = this.state.todos.concat(this.state.todoToAdd);
+    const nextTodoToAdd = "";
+    
+    this.setState({
+      todos: nextTodos,
+      todoToAdd: nextTodoToAdd
+    });
   }
 
   render(){
@@ -20,29 +44,36 @@ class App extends Component {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          This is a new line, i'm just excited to carry on.
-          What else?
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <a
-          className="App-link"
-          href="https://google.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Link to google
-        </a>
+        <h2>
+          Welcome to my toDo List
+        </h2>        
+        <ul>
+          {this.state.todos.map((todo,index)=>
+          <li key={index}>{todo}</li>)}
+        </ul>
+
+        <input type="text" 
+        placeholder="New Todo"
+        value={this.state.todoToAdd}
+        onChange={this.handleChange}
+        /**
+         * If you dont want to 'bind' this you can implement the handleChange as an arrow function:
+         *      handleChange= (evt)=>{...}
+         * and then call the function normally
+         *      onChange={this.handleChange}
+         * 
+         *Alternatively you can use an arrow function:
+         *      onChange={()=>this.handleChange}
+         * 
+         * The problem with this syntax is that a different callback is created each time the 
+         * handleChange renders. In most cases, this is fine. However, if this callback is passed 
+         * as a prop to lower components, those components might do an extra re-rendering.
+         *  
+         * We generally recommend binding in the constructor or using the class fields syntax, 
+         * to avoid this sort of performance problem.
+         * */        
+        />
+        <button onClick={this.handleClick}>Add Todo</button>
       </header>
     </div>
   );
