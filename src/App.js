0e156f50d869043e44e40920a3d234ec58ba4164
Rasec <mac@Macs-MacBook-Air.local>
2021-02-01 10:18:34 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state={
+      todos: [
+        'Learn React',
+        'Do Laundry'
+      ]
+    };
+  }
+
+  render(){
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          This is a new line, i'm just excited to carry on.
+          What else?
         </p>
         <a
           className="App-link"
@@ -17,9 +35,18 @@ function App() {
         >
           Learn React
         </a>
+        <a
+          className="App-link"
+          href="https://google.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Link to google
+        </a>
       </header>
     </div>
   );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,11 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const name = 'Cesar Herrera';
+const element=<h1>Hello, {name}</h1>;
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <p>
+      This is another line test
+    </p>
+  {element}
   </React.StrictMode>,
   document.getElementById('root')
+  /**
+   * This is jsx syntax and will be translated to:
+   * ReactDOM.render(React.createElement(App, null), document.getElementById('root'));
+   */
 );
 
 // If you want to start measuring performance in your app, pass a function
