8fb40c127b41af88b80e2275a8c89f8a8566b0a2
Rasec <elch.herrera@gmail.com>
2021-02-02 18:36:02 -0500

    add checkBox event logic

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -2,7 +2,16 @@ import React, { Component } from 'react';
 
 class TodoItem extends Component {
   render(props) {
-    return <li><input type="checkbox" checked={this.props.isCompleted} />{this.props.children}</li>
+    return (
+    <li>
+      <input 
+        type="checkbox" 
+        checked={this.props.isCompleted}
+        onChange={(event) => this.props.onToggleTodo(this.props.index, event.target.checked)}
+      />
+      {this.props.children}
+    </li>
+    )
   }
 }
 
